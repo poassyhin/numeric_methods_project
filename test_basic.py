06094279b632c91071969877a68d8c08f94de396
poassyhin <76241948+poassyhin@users.noreply.github.com>
2022-12-26 23:39:37 +0300

    Update test_basic.py

--- test_basic.py
+++ test_basic.py
@@ -3,7 +3,7 @@ def solve(f, a, b, e):
   print(bin_search.dichotomy(f, a, b, e))
 
 def main():
-  solve(x**3-x+1, 0, 10, 0.0001)
+  solve('x**3-x+1', -10, 10, 0.0001)
 
 if __name__ == "__main__":
   main()
