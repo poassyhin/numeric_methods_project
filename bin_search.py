f9b6c98146919d93a3590d1bc01a26798f0c9624
smrnminion <32909807+smrnminion@users.noreply.github.com>
2022-12-26 23:42:49 +0300

    Update bin_search.py

--- bin_search.py
+++ bin_search.py
@@ -7,7 +7,7 @@ def dichotomy (fun, a,b, eps):
     n = 1000
     res = []
     setka=np.linspace(a, b, n)
-
+    
     for x,y in zip(setka, setka[1:]):
         if f(fun,x) * f(fun,y) > 0:
             continue
@@ -15,12 +15,21 @@ def dichotomy (fun, a,b, eps):
         while ( abs(f(fun,y)-f(fun,x)) )>eps:   
             mid = (y+x)/2              
             if f(fun,mid) == 0 or f(fun,mid)<eps:  
-                root = mid                
+                root = mid
                 break
             elif (f(fun,mid) * f(fun,x)) < 0:   
                 y = mid            
             else:
-                x = mid          
-        if root != None:
+                x = mid
+        if(root == None):
+            mid = (y+x)/2              
+            if f(fun,mid) == 0 or f(fun,mid)<eps:  
+                root = mid
+
+            elif (f(fun,mid) * f(fun,x)) < 0:   
+                y = mid            
+            else:
+                x = mid
+        if(root != None):
             res.append(root)
     return res
